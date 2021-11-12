60a98019ffb83efd7ee48f789b4d60a307171b01
Dan Ngyen <wpcodev@gmail.com>
2021-11-13 00:23:31 +0700

    add css

--- footer.php
+++ footer.php
@@ -16,21 +16,21 @@
 <div class="tool">
 	<ul>
 		<li>
-			Set Font Content:<br />
+			Set Font Content
 			<select class="content-font">
 				<?php
 				foreach($fonts as $key=>$font){?>
-				<option value="<?php echo $key;?>"><?php echo $key;?></option>
+				<option value="<?php echo $key;?>" <?php if($content_font == $key) echo 'selected';?> ><?php echo $key;?></option>
 				<?php } ?>
 
 			</select>
 		</li>
 		<li>
-			Set Font Content Label:<br />
+			Set Font Content Label
 			<select class="content-label">
 				<?php
 				foreach($fonts as $key=>$font){?>
-				<option value="<?php echo $key;?>"><?php echo $key;?></option>
+				<option value="<?php echo $key;?>" <?php if($label_font == $key) echo 'selected';?> ><?php echo $key;?></option>
 				<?php } ?>
 
 			</select>
