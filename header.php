905454d1952cab9dee8e28e97ff3ee148663092a
Dan Ngyen <wpcodev@gmail.com>
2021-11-13 14:28:41 +0700

    udpate css

--- header.php
+++ header.php
@@ -8,7 +8,8 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>CV Online</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="bootstrap.min.css"  rel="stylesheet" >
+
         <link href="style.css?ver=<?php echo rand();?>" rel="stylesheet" >
 
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Montserrat:wght@200;300;400;500&family=Open+Sans:wght@400;500&family=Oswald:wght@200;300;400;500&family=Poppins:wght@200;300;400;500&family=Raleway:wght@200;300;400;500&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300&family=Roboto:wght@300;400&family=Rubik:wght@300;400;500&display=swap" rel="stylesheet">
